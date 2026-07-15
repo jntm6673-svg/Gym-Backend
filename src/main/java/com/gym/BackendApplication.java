7879f9927ffca77345cd93090cbdfafa7847e500
jntm6673-svg <jntm6673@gmail.com>
2026-07-15 10:00:12 +0800

    健身房管理系统后端

---
+++ src/main/java/com/gym/BackendApplication.java
@@ -0,0 +1,30 @@
+package com.gym;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * SpringBoot 启动类
+ * 项目启动入口（点击右键 选择 "run BackendApplication" 启动项目）
+ * <p>
+ * 什么是SpringBoot？（https://www.php.cn/faq/498384.html）
+ *
+ * @author: ShanZhu
+
+ */
+@Slf4j
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        //SpringBoot 执行启动
+        SpringApplication.run(BackendApplication.class, args);
+
+//        log.info("=====================项目后端启动成功============================");
+    }
+
+}
+//redis-server.exe redis.windows.conf
+//taskkill /F /IM nginx.exe
---
+++ src/main/java/com/gym/entity/Course.java
@@ -0,0 +1,40 @@
+package com.gym.entity;
+
+import lombok.Data;
+
+/**
+ * 课程 实体
+ *
+ * @author: ShanZhu
+ * @date: 2024-08-01
+ */
+@Data
+public class Course {
+
+    private Integer courseNo;
+
+    private String courseName;
+
+    private String courseTime;
+
+    private String courseDuration;
+
+    private double coursePrice;
+
+    private String courseDesc;
+
+    private int courseIntegral;
+
+    private Integer employeeNo;
+
+    private Integer managerNo;
+
+    private String employeeNameCoach;
+
+    private String employeePhoneCoach;
+
+    private String employeeNameManager;
+
+    private String employeePhoneManager;
+
+}
